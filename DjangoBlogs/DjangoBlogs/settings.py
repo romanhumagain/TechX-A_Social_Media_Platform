ebdbce47569f7300ce0055ebda76b79896d0970f
Roman Humagain <127822516+romanhumagain@users.noreply.github.com>
2023-08-15 00:20:15 -0700

    updated email smtp code

--- DjangoBlogs/DjangoBlogs/settings.py
+++ DjangoBlogs/DjangoBlogs/settings.py
@@ -145,5 +145,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_URL = 'login_user'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'romanhmgn6999@gmail.com'
+EMAIL_HOST_PASSWORD = 'hrzvdgdqqqzuehjz'
+EMAIL_USE_TLS = True
