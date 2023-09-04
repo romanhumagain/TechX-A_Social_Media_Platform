ea705083e690b371d37a23772d0f0955d8a11fbf
romanhumagain <romanhumagain@gmail.com>
2023-09-04 03:02:52 -0700

    modified crispy form

--- DjangoBlogs/Blog/forms.py
+++ DjangoBlogs/Blog/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from . models import BlogPost
 
+
 class BlogPostForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}))
+    title = forms.CharField(
+        label="Title of the Post",
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'style':'text-transform: capitalize'
+                                      }))
+    
+    content = forms.CharField(
+        label="Post Content",
+        widget=forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}))
     image = forms.ImageField(required=False)
+    
     class Meta:
         model = BlogPost
         fields = ['title', 'content' , 'image']
