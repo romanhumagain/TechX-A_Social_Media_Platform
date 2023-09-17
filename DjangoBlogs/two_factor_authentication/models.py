afb968fa8323f1407590490008bdedfdcf4406ff
romanhumagain <romanhumagain@gmail.com>
2023-09-17 02:14:46 -0700

    configured two factor authentication

--- DjangoBlogs/two_factor_authentication/models.py
+++ DjangoBlogs/two_factor_authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TwoFactorToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='two_factor_token')
+    totp_key = models.CharField(max_length=50, blank=True, null=True) 
+    last_verified_otp = models.IntegerField(blank=True, null=True)  
+    is_verified = models.BooleanField(default=False)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def generate_otp(self):
+        pass
+    
+    def verify_otp(self, otp):
+        pass
