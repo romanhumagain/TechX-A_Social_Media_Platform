2573a1dc6f104ef78e58b0be27dcf03278fb2df4
romanhumagain <romanhumagain@gmail.com>
2023-09-17 22:00:52 -0700

    configured two factor authentication

--- DjangoBlogs/two_factor_authentication/models.py
+++ DjangoBlogs/two_factor_authentication/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class TwoFactorToken(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='two_factor_auth_token')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='two_factor')
     totp_key = models.CharField(max_length=50, blank=True, null=True)  
     last_verified_otp = models.IntegerField(blank=True, null=True)  
-    is_verified = models.BooleanField(default=False)  
-    has_enabled_2fa = models.BooleanField(default=False)  
+    is_verified = models.BooleanField(default=False)   
     date_2fa_enabled = models.DateTimeField(blank=True, null=True)  
     failed_attempts = models.IntegerField(default=0)  
     backup_codes = models.TextField(blank=True, null=True)  
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    date_2fa_disabled = models.DateTimeField(blank=True, null=True)  
     
     def generate_otp(self):
         pass
