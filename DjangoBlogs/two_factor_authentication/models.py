d8db218aa4c1acc3f856f98fc4d1352367eb52b6
romanhumagain <romanhumagain@gmail.com>
2023-09-17 23:18:40 -0700

    changed model

--- DjangoBlogs/two_factor_authentication/models.py
+++ DjangoBlogs/two_factor_authentication/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils import timezone
+from datetime import timedelta
+
 class TwoFactorToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='two_factor')
     totp_key = models.CharField(max_length=50, blank=True, null=True)  
@@ -24,3 +27,21 @@ class TwoFactorToken(models.Model):
 
     def validate_backup_code(self, code):
         pass
+
+
+class OTPVerification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='otp_verifications')
+    otp = models.CharField(max_length=8)  # Assuming 6-digit OTP
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(default=timezone.now() + timedelta(minutes=10))
+    
+    def is_valid(self):
+        return timezone.now() < self.expires_at
+
+    def verify(self, provided_otp):
+        if self.is_valid() and self.otp == provided_otp:
+            self.is_verified = True
+            self.save()
+            return True
+        return False
